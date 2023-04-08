90689dee2e0cfcf57472dd68bf7876f53bdedea7
Aahan Suneja <aahan.suneja@gmail.com>
2023-04-08 03:27:09 +0000

    Added update script

---
+++ update-blocklist.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd generate-domains-blocklist
+python3 generate-domains-blocklist.py -o ~/dnscrypt/blocked-names.txt
+cd ..
+git add blocked-names.txt
+git commit -m "Last updated: $(date +%F)"
+git push
